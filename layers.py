425b2930917eb940f4c076f74e8afc866486018e
Zach Maurer <zach.maurer@gmail.com>
2017-05-10 11:32:55 -0700

    Started modularizing code, utils.py code is nonfunctional.

---
+++ layers.py
@@ -0,0 +1,6 @@
+import torch.nn as nn
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        N, C, H, W = x.size() # read in N, C, H, W
+        return x.view(N, -1)  # "flatten" the C * H * W values into a single vector per image
