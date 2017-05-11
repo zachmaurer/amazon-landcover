f42f408028908e06833c28b39910ec4ab27d710f
Tanuj <tanuj@stanford.edu>
2017-05-11 10:18:48 +0000

    out of memory error occuring when I load too much data (>3000 jpeg files). need to fix. can use naive dataset to get going

--- data_utils.py
+++ data_utils.py
@@ -99,6 +99,8 @@ class DynamicDataset(Dataset):
         self.data_tensor = np.zeros((buffer_size,3,256,256))
         self.backup_buffer = np.zeros(self.data_tensor.shape)
         
+        self.buffer_flag = -1
+        
         for i in range(self.buffer_index*buffer_size,self.buffer_index*buffer_size+1):
             self.data_tensor[i,:,:,:] = self.load_image(i)
         self.data_tensor = torch.from_numpy(self.data_tensor)
@@ -111,30 +113,46 @@ class DynamicDataset(Dataset):
         return im  
     
     def fill_buffer(self):
-        self.backup_buffer = np.zeros((self.buffer_size,3,256,256)) #does this clear the GPU RAM properly? Monitor memory..
-        self.buffer_index += 1
-        for i in range(int(self.buffer_size)):
-            self.backup_buffer[i,:,:,:] = self.load_image(i+self.buffer_index*self.buffer_size)
-        #self.backup_buffer = torch.from_numpy(self.backup_buffer) #should this happen halfway, or at the end?
+        if self.buffer_index%2 == 0:
+            del self.backup_buffer
+            backup_buffer_np = np.zeros((self.buffer_size,3,256,256)) #does this clear the GPU RAM properly? Monitor memory..
+            for i in range(int(self.buffer_size)):
+                backup_buffer_np[i,:,:,:] = self.load_image(i+self.buffer_index*self.buffer_size)
+            self.backup_buffer = torch.from_numpy(backup_buffer_np) #should this happen halfway, or at the end?
+            del backup_buffer_np
+        else:
+            del self.data_tensor
+            data_tensor_np = np.zeros((self.buffer_size,3,256,256)) #does this clear the GPU RAM properly? Monitor memory..
+            for i in range(int(self.buffer_size)):
+                data_tensor_np[i,:,:,:] = self.load_image(i+self.buffer_index*self.buffer_size)
+            self.data_tensor = torch.from_numpy(data_tensor_np) #should this happen halfway, or at the end?
+            del data_tensor_np
 
     def __getitem__(self, index):
-        if index>self.buffer_index*self.buffer_size/2:
+        if index>(1+self.buffer_index)*self.buffer_size/2:
             self.fill_buffer()
-        elif index>int(3*self.buffer_index*self.buffer_size/4):
-            self.backup_buffer = torch.from_numpy(self.backup_buffer)
-        elif index>=self.buffer_index*self.buffer_size:
-            self.data_tensor = self.backup_buffer #does this do assignment properly w/o causing a GPU/CPU RAM memory leak?
-        return self.data_tensor[index%self.buffer_size], self.labels_tensor[index%self.buffer_size]
+        #elif index>int(3*self.buffer_index*self.buffer_size/4):
+        #    self.backup_buffer = torch.from_numpy(self.backup_buffer)
+        if index>=self.buffer_index*self.buffer_size:
+            self.buffer_index += 1
+        if self.buffer_index%2 == 1:
+            return self.data_tensor[index%self.buffer_size], self.labels_tensor[index%self.buffer_size]
+        else:
+            return self.backup_buffer[index%self.buffer_size], self.labels_tensor[index%self.buffer_size]
+            #self.data_tensor = self.backup_buffer #does this do assignment properly w/o causing a GPU/CPU RAM memory leak?
+        #return self.data_tensor[index%self.buffer_size], self.labels_tensor[index%self.buffer_size]
 
     def __len__(self):
-        return self.data_tensor.size(0)
+        return self.num_examples
 
 #Since dataloaders are created in conjunction with samplers, and because of our RAM constraint when loading data,
 #We needed to create this helper function to produce a dataloader object with the appropraite sampler. Without this helper
 #function, there is a risk that the Sampler would not be able to sample random pictures properly
 def createFastLoaderWithSampler(data_path=train_data_path, labels_path=train_labels_path,
-                 num_examples=1000, buffer_size=1000, rand_seed = None, batch_size=100):
+                 num_examples=1000, buffer_size=1000, rand_seed = None, batch_size=100, num_workers=8):
     dynamic_dataset = DynamicDataset(data_path, labels_path,
                  num_examples, buffer_size, rand_seed)
+    print(len(dynamic_dataset))
     return torch.utils.data.DataLoader(dynamic_dataset, batch_size=100, 
-                                       shuffle=(rand_seed is not None), sampler=SequentialSampler(dynamic_dataset))
+                                       shuffle=(rand_seed is not None), sampler=SequentialSampler(dynamic_dataset),
+                                       num_workers=num_workers)
